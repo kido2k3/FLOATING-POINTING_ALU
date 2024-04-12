bc8798397b1a1923f65b53dc0ca7269e38ef3230
kido2k3 <hoangphongvip2003@gmail.com>
2024-04-12 07:49:21 +0700

    init commit

---
+++ MultiOp.v
@@ -0,0 +1,17 @@
+// Multiplication operators between two floating-point numbers
+module MultiOp (
+    output [31:0] out,
+    output under_overflow,
+    input [31:0] para1,
+    input [31:0] para2
+);
+// initialize variables
+
+
+// mutiply fraction elements
+
+// calculate exponent parts
+
+
+//
+endmodule
